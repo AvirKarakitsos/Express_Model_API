96b79f154c4c7a7517b9f2bf965f00a11b55f12f
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-09-20 11:52:09 +0200

    feat: controller and route files

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import { logger } from './middleware/logEvents.js';
 import { errorHandler } from './middleware/errorHandler.js';
+import routeApi from './routes/api.js';
 
 const PORT = 4000;
 
 const server = express();
 
 server.use(logger);
-server.get('/', (req, res) => {
-    res.status(200).json('test 2');
-});
+
+server.use('/api', routeApi);
 
 server.use(errorHandler);
 
 server.listen(PORT, () => {
-    console.log('Server listening in port: ' + PORT);
+    console.log('Server listening on port: ' + PORT);
 });
